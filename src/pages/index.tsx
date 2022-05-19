63798b0eaacf53d5b8c8217cc8352bcb53769e81
Davi Lacerda <dlacerda9@gmail.com>
2022-05-19 19:28:44 -0300

    Initial Commit: create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Footer } from "../components/Footer";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Davi Lacerda | Front-End Developer</title>
+            </Head>
+        </>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "brand": {
+                    "hover": "#996DFF",
+                    DEFAULT: "#8257E5",
+                },
+                "surface":{
+                    "primary": colors.zinc["900"],
+                    "secondary": colors.zinc["800"],
+                    "secondary-hover": colors.zinc["700"],
+                },
+                "background":{
+                    DEFAULT: "#18181B",
+                }
+            },
+            fontFamily: {
+                inter: ["Inter", "sans-serif"],
+                play: ["Play, sans-serif"],
+                fira: ["Fira Code", "monospace"],
+            },
+            textColor:{
+                "primary": colors.zinc["100"],
+                "secondary": colors.zinc["400"],
+                "tooltip": colors.zinc["800"],
+            },
+        },
+    },
+    plugins: [],
+};
