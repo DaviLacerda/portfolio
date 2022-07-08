08fe3029aa8fa156ad8929198d2a2a4c3ae85b4d
Davi Lacerda <dlacerda9@gmail.com>
2022-07-08 20:14:57 -0300

    update: change responsive console slider

--- src/components/ConsoleCircle/index.tsx
+++ src/components/ConsoleCircle/index.tsx
@@ -4,6 +4,6 @@ interface ConsoleCircleProps {
 
 export function ConsoleCircle({ color }: ConsoleCircleProps) {
     return (
-        <div className={`h-3 w-3 md:w-6 md:h-6 ${color} rounded-full`} />
+        <div className={`hidden md:block w-6 h-6 ${color} rounded-full`} />
     )
 }
